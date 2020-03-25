9ccc0f4eaab11997d9939358af1bc867e0e9f670
rlhatch <61712432+rlhatch@users.noreply.github.com>
2020-03-25 10:20:27 -0600

    added new code for tests from RH

--- test_runner.js
+++ test_runner.js
@@ -15,22 +15,22 @@ function runTests() {
     // check that adding input and clicking add will add a word
 
     input.value = 'test1';
-    addButton.click;
-    testsPassed.push(collection.value === 'test1');
+    addButton.click();
+    testsPassed.push(collection.innerHTML === 'test1');
 
     // check that adding another piece of input will add a second word concatenated with ", "
 
     input.value = 'test2';
-    addButton.click;
-    testsPassed.push(collection.value === 'test1, test2');
+    addButton.click();
+    testsPassed.push(collection.innerHTML === 'test1, test2');
 
     // check that clear button exists
     testsPassed.push(clearButton !== null)
 
     // check that clicking clear will remove all items from collection
     if(clearButton !== null){
-        clearButton.click;
-        testsPassed.push(collection.value === '');
+        clearButton.click();
+        testsPassed.push(collection.innerHTML === '');
     }
 
 
